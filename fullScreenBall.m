d40c4bfbb1b9c263e2b75f92066fbf810acdc575
climber59 <42081329+climber59@users.noreply.github.com>
2020-09-14 16:26:13 -0400

    Update fullScreenBall.m
    comments

--- fullScreenBall.m
+++ fullScreenBall.m
@@ -10,17 +10,17 @@ function [] = fullScreenBall()
 	g = groot;
 	screen = get(0,'ScreenSize');
 	
-	dt = 0.001;
-	v = 1000;
-% 	dt = 0.1;
+	dt = 0.01;
+	v = 6;
 
 	n = 4;
 	
+	fprintf('\n\nMove the 4 figures around to follow the ball\nClose any figure to stop\n')
 	figureSetup();
 	run();
 	
 	function [] = run()
-		while all(ishandle(f))
+		while all(ishandle(f)) % run while all figures still exist
 			
 			vx = ball.UserData.v*cos(ball.UserData.t);
 			vy = ball.UserData.v*sin(ball.UserData.t);
@@ -30,19 +30,19 @@ function [] = fullScreenBall()
 			x = ball.UserData.x + dx;
 			y = ball.UserData.y + dy;
 			
-			if x<=0 || x>=screen(3) %bounce on right/left wall
+			if x<=0 || x>=screen(3) %bounce on right/left edge of the screen (I don't know how it works on dual monitors)
 				vx = -vx;
 			end
-			if y>=screen(4) || y<=0 % bounce on bot/top wall
+			if y>=screen(4) || y<=0 % bounce on bot/top edge of the screen
 				vy = -vy;
 			end
 			
 			
 			figOrder = [g.Children.Number];
-			overlap = ballInRect(figOrder);
+			overlap = ballInRect(figOrder); % which figures is the ball within
 			i = figOrder(find(overlap,1));
 			if isempty(i)
-				ball.Visible = 'off';
+				ball.Visible = 'off'; % just make it invisible if it should be off-figure
 			else
 				% put ball on f(i)
 				xa = x - f(i).Position(1);
@@ -50,7 +50,7 @@ function [] = fullScreenBall()
 				ball.XData = ball.UserData.px + xa;
 				ball.YData = ball.UserData.py + ya;
 
-				if ball.UserData.axis ~= i || strcmp(ball.Visible, 'off')
+				if ball.UserData.axis ~= i || strcmp(ball.Visible, 'off') % make it visible and store the current figure index if this is the first time it appeared
 					ball.Parent = ax(i);
 					ball.UserData.axis = i;
 					ball.Visible = 'on';
@@ -67,6 +67,7 @@ function [] = fullScreenBall()
 		end
 	end
 	
+	% create the figures and the ball
 	function [] = figureSetup()
 		f = gobjects(n,1);
 		ax = gobjects(n,1);
@@ -94,16 +95,17 @@ function [] = fullScreenBall()
 		t=linspace(0,2*pi,24);
 		x = cos(t)*7;
 		y = sin(t)*7;
-		ball = patch(f(1).Position(3)/2+x,f(1).Position(4)/2+y,[0 0 0]);
+		ball = patch(f(1).Position(3)/2+x,f(1).Position(4)/2+y,[0 0 0]); % start within figure 1
 		ball.UserData.x = f(1).Position(3)/2 + f(1).Position(1);
 		ball.UserData.y = f(1).Position(4)/2 + f(1).Position(2);
-		ball.UserData.v = v;
+		ball.UserData.v = v/dt;
 		ball.UserData.t = 2*pi*rand;
 		ball.UserData.px = x;
 		ball.UserData.py = y;
 		ball.UserData.axis = 1;
 	end
 	
+	% returns a bool array. true if the ball is within a figure
 	function [o] = ballInRect(fig)
 		x = ball.UserData.x;
 		y = ball.UserData.y;
@@ -115,6 +117,7 @@ function [] = fullScreenBall()
 		end
 	end
 	
+	% rescale axes when the figure changes size
 	function [] = resize(src,~)
 		a = src.Children;
 		a.XLim(2) = src.Position(3);
